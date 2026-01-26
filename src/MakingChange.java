cf2237ea34628a9c245da8d9db5ff103cb3e66a6
L-Tran <logan.tran@menloschool.org>
2026-01-25 17:41:18 -0800

    Attempted memoization

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -8,10 +8,41 @@
 
 public class MakingChange {
 
+    public static int[] ways;
+
     public static long countWays(int target, int[] coins) {
         // Array representing the # of ways for the index amount of coins
-        int[] ways = new int[coins.length + 1];
-        return 0;
+        ways = new int[target + 1];
+        for(int i = 0; i <= target; i++) {
+            ways[i] = -1;
+        }
+        return waysPerCoin(target, coins);
+    }
+
+    public static int waysPerCoin(int target, int[] coins) {
+        if(target == 0) {
+            return 1;
+        }
+
+        if(target < 0) {
+            return 0;
+        }
+
+        if(ways[target] != -1) {
+            return ways[target];
+        }
+
+        int total = 0;
+
+        for(int coin: coins) {
+            total += waysPerCoin(target - coin, coins);
+        }
+
+        ways[target] = total;
+        return total;
+
+
+
     }
 
 }
