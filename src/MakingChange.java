4db03c161a4ee59fc983489375e9c8714bdb6fac
L-Tran <logan.tran@menloschool.org>
2026-01-30 13:29:29 -0800

    Final commit

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -16,50 +16,53 @@ public class MakingChange {
         for(int i = 0; i < coins.length; i++) {
             ways[i][0] = 1;
         }
-//        long include;
-//        long exclude;
-//        for(int i = 0; i < coins.length; i++) {
-//            for (int j = 1; j < target + 1; j++) {
-//                include = 0;
-//                exclude = 0;
-//                if(j - coins[i] >= 0) {
-//                    include = ways[i][j - coins[i]];
-//                }
-//                if(i - 1 >= 0) {
-//                    exclude = ways[i - 1][j];
-//                }
-//                ways[i][j] = include + exclude;
-//            }
-//        }
+        long include;
+        long exclude;
+        // Loop through ways array
+        for(int i = 0; i < coins.length; i++) {
+            for (int j = 1; j < target + 1; j++) {
+                // Set include and exclude to default to out of bounds
+                include = 0;
+                exclude = 0;
+                // If indexes are in bounds
+                if(j - coins[i] >= 0) {
+                    // Set include value to included amount
+                    include = ways[i][j - coins[i]];
+                }
+                if(i - 1 >= 0) {
+                    // Set exclude value to excluded amount
+                    exclude = ways[i - 1][j];
+                }
+                // The sum of include & exclude for an index is the amount of ways
+                ways[i][j] = include + exclude;
+            }
+        }
 
         // Recursive call for Memoization
-        count(target, coins.length - 1, coins);
+//        count(target, coins.length - 1, coins);
+
         return ways[coins.length - 1][target];
-
-
     }
 
     // Memoization
-    public static long count(int sum, int index, int[] coins) {
-        // If sum is perfect cut down it is a solution
-        if(sum == 0) {
-            return 1;
-        }
-        // Ensure within bounds
-        if(index < 0 || sum < 0) {
-            return 0;
-        }
-        // Use memoization if possible
-        if(ways[index][sum] != 0) {
-            return ways[index][sum];
-        }
-
-        // Update memoization
-        ways[index][sum] = count(sum - coins[index], index, coins) + count(sum, index - 1, coins);
-
-        return ways[index][sum];
-
-
-    }
+//    public static long count(int sum, int index, int[] coins) {
+//        // If sum is perfect cut down it is a solution
+//        if(sum == 0) {
+//            return 1;
+//        }
+//        // Ensure within bounds
+//        if(index < 0 || sum < 0) {
+//            return 0;
+//        }
+//        // Use memoization if possible
+//        if(ways[index][sum] != 0) {
+//            return ways[index][sum];
+//        }
+//
+//        // Update memoization
+//        ways[index][sum] = count(sum - coins[index], index, coins) + count(sum, index - 1, coins);
+//
+//        return ways[index][sum];
+//    }
 
 }
