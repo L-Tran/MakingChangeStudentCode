ed8096d399456018a1eb4bfc7650ec36cc91a224
L-Tran <logan.tran@menloschool.org>
2026-01-30 13:11:19 -0800

    Finished tabulation

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -16,33 +16,48 @@ public class MakingChange {
         for(int i = 0; i < coins.length; i++) {
             ways[0][i] = 1;
         }
+        long include;
+        long exclude;
+        for(int i = 1; i < target + 1; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                include = 0;
+                exclude = 0;
+                if(i - coins[j] >= 0) {
+                    include = ways[i - coins[j]][j];
+                }
+                if(j - 1 >= 0) {
+                    exclude = ways[i][j - 1];
+                }
+                ways[i][j] = include + exclude;
+            }
+        }
 
         // Recursive call for Memoization
-        count(target, coins.length - 1, coins);
+//        count(target, coins.length - 1, coins);
         return ways[target][coins.length - 1];
     }
 
     // Memoization
-    public static long count(int sum, int index, int[] coins) {
-        // If sum is perfect cut down it is a solution
-        if(sum == 0) {
-            return 1;
-        }
-        // Ensure within bounds
-        if(index < 0 || sum < 0) {
-            return 0;
-        }
-        // Use memoization if possible
-        if(ways[sum][index] != 0) {
-            return ways[sum][index];
-        }
+//    public static long count(int sum, int index, int[] coins) {
+//        // If sum is perfect cut down it is a solution
+//        if(sum == 0) {
+//            return 1;
+//        }
+//        // Ensure within bounds
+//        if(index < 0 || sum < 0) {
+//            return 0;
+//        }
+//        // Use memoization if possible
+//        if(ways[sum][index] != 0) {
+//            return ways[sum][index];
+//        }
+//
+//        // Update memoization
+//        ways[sum][index] = count(sum - coins[index], index, coins) + count(sum, index - 1, coins);
+//
+//        return ways[sum][index];
 
-        // Update memoization
-        ways[sum][index] = count(sum - coins[index], index, coins) + count(sum, index - 1, coins);
 
-        return ways[sum][index];
-
-
-    }
+//    }
 
 }
